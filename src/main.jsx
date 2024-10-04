13ac8926ec55c2fdbf860e2e4a6e377814470c33
Jean Tessier <jean@jeantessier.com>
2024-10-03 17:18:46 -0700

    Use React Router and NotFound page
    https://reactrouter.com/en/main/start/tutorial

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import App from './App.jsx'
+import Notfound from "./routes/notfound"
 import './index.css'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App/>,
+        errorElement: <Notfound/>,
+    }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/routes/notfound/NotFound.jsx
@@ -0,0 +1,7 @@
+import './NotFound.css'
+
+export default function NotFound() {
+    return (
+        <p>There is nothing here!</p>
+    )
+}
