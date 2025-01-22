5c70bd7ba8fe3b4521d4bfb2d41cb94d03cf82b1
Jean Tessier <jean@jeantessier.com>
2025-01-22 10:06:25 -0800

    Fix lint issue

--- src/routes/cycles/CyclesControls.jsx
+++ src/routes/cycles/CyclesControls.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { CYCLES_URL, INBOUND, OUTBOUND } from '../../lib/constants'
+import { CYCLES_URL } from '../../lib/constants'
 import Title from '../../shared/Title'
 import './CyclesControls.css'
 
