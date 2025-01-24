4b817f2e066a809943fbcf6022d7226df16b9261
Jean Tessier <jean@jeantessier.com>
2025-01-23 21:44:39 -0800

    Use Apache ECharts to render chart and histograms
    https://echarts.apache.org/
    
        npm install --save echarts-for-react
    
    Chart only has series that have data in them (some non-zero values).
    
    Empty histograms are hidden.

--- src/routes/metrics/Chart.jsx
+++ src/routes/metrics/Chart.jsx
@@ -1,10 +1,58 @@
 import PropTypes from 'prop-types'
+import ReactECharts from 'echarts-for-react'
 import './Chart.css'
 
 function Chart({ chart }) {
+    if (!chart) {
+        return (
+            <></>
+        )
+    }
+
+    // Chart's first row is headers for the various series.
+    // We skip it when building series by calling ''chart.slice(1)''.
+
+    const xAxisData = chart.slice(1).map(point => point[0])
+
+    // Chart's first column is the x-value.
+    // When we build ''series'' below, it has an entry for each
+    // column.  We skip the first column with ''.slice(1)'' and
+    // then we filter out empty series (all zeroes).
+
+    const series = chart[0].map((name, i) => {
+        return {
+            name,
+            type: 'line',
+            smooth: true,
+            data: chart.slice(1).map(point => point[i]),
+        }
+    }).slice(1).filter(series => series.data.find(point => point[1] !== 0))
+
+    const chartOptions = {
+        title: {
+            text: 'Metrics',
+        },
+        legend: {
+            type: 'plain',
+            orient: 'vertical',
+        },
+        xAxis: {
+            data: xAxisData,
+            name: 'cardinality',
+            nameLocation: 'middle',
+            nameGap: 25,
+        },
+        yAxis: {
+            name: 'size',
+            nameLocation: 'middle',
+            nameGap: 25,
+        },
+        series,
+    }
+
     return (chart &&
             <div className="chart">
-                Chart goes here ...
+                <ReactECharts option={chartOptions} />
             </div>
     )
 }
--- src/routes/metrics/Histogram.jsx
+++ src/routes/metrics/Histogram.jsx
@@ -1,10 +1,44 @@
 import PropTypes from 'prop-types'
 import './Histogram.css'
+import ReactECharts from "echarts-for-react";
 
 function Histogram({ title, histogram }) {
+    if (!histogram || histogram.length === 0) {
+        return (
+            <></>
+        )
+    }
+
+    const xAxisData = histogram.map(point => point[0])
+    const seriesData = histogram.map(point => point[1])
+
+    const chartOptions = {
+        title: {
+            text: title,
+        },
+        xAxis: {
+            data: xAxisData,
+            name: 'cardinality',
+            nameLocation: 'middle',
+            nameGap: 25,
+        },
+        yAxis: {
+            name: 'size',
+            nameLocation: 'middle',
+            nameGap: 25,
+        },
+        series: [
+            {
+                type: 'line',
+                smooth: true,
+                data: seriesData,
+            },
+        ],
+    }
+
     return (
         <div className="histogram">
-            Histogram <b>{title}</b> goes here...
+            <ReactECharts option={chartOptions} />
         </div>
     )
 }
