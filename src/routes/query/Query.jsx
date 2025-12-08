f03279c4c4054a028618882cc50496643733e92a
Jean Tessier <jean@jeantessier.com>
2025-12-07 20:49:51 -0800

    Navigate by clicking on query results

--- src/routes/query/Query.jsx
+++ src/routes/query/Query.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
 import { Footer } from '../../shared'
 import { useTitle } from '../../hooks'
 import QueryControls from './QueryControls'
@@ -8,16 +9,54 @@ import './Query.css'
 const Query = () => {
     useTitle('Query')
 
+    const [searchParams, setSearchParams] = useSearchParams()
+    const getStringParams = (name, defaultValue) => searchParams.has(name) ? searchParams.get(name) : defaultValue
+    const getBooleanParams = (name, defaultValue) => searchParams.has(name) ? searchParams.get(name) === 'true' : defaultValue
+
+    const initForm = {
+        scopeIncludes: getStringParams('scopeIncludes', "//"),
+        scopeExcludes: getStringParams('scopeExcludes', ""),
+        packageScope: getBooleanParams('packageScope', true),
+        classScope: getBooleanParams('classScope', false),
+        featureScope: getBooleanParams('featureScope', false),
+        filterIncludes: getStringParams('filterIncludes', "//"),
+        filterExcludes: getStringParams('filterExcludes', ""),
+        packageFilter: getBooleanParams('packageFilter', true),
+        classFilter: getBooleanParams('classFilter', false),
+        featureFilter: getBooleanParams('featureFilter', false),
+        showInbounds: getBooleanParams('showInbounds', true),
+        showOutbounds: getBooleanParams('showOutbounds', true),
+        showEmptyNodes: getBooleanParams('showEmptyNodes', false),
+    }
+
+    const [form, setForm] = useState(initForm)
     const [queryResults, setQueryResults] = useState([])
+
+    const handleTextChange = e => {
+        const updatedForm = { ...form, [e.target.name]: e.target.value }
+
+        setForm(updatedForm)
+        setSearchParams(updatedForm)
+    }
+
+    const handleFlagChange = e => {
+        const updatedForm = { ...form, [e.target.name]: e.target.checked }
+
+        setForm(updatedForm)
+        setSearchParams(updatedForm)
+    }
 
     const onNameClick = name => {
         const regex = `/^${name.replaceAll(/([$()[\]])/g, '\\$1')}/`
-        console.log(regex)
+        const updatedForm = { ...form, scopeIncludes: regex }
+
+        setForm(updatedForm)
+        setSearchParams(updatedForm)
     }
 
     return (
         <>
-            <QueryControls setQueryResults={setQueryResults} />
+            <QueryControls form={form} handleTextChange={handleTextChange} handleFlagChange={handleFlagChange} setQueryResults={setQueryResults} />
             <QueryResults queryResults={queryResults} type="packages" onNameClick={onNameClick} />
             <Footer />
         </>
--- src/routes/query/QueryControls.jsx
+++ src/routes/query/QueryControls.jsx
@@ -1,114 +1,36 @@
 import PropTypes from 'prop-types'
-import { useEffect, useState } from 'react'
-import { useForm } from 'react-hook-form'
-import { Link, useSearchParams } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { useVersion } from '../../hooks'
 import { QUERY_URL, INBOUND, OUTBOUND } from '../../lib'
 import { NavBar, Title } from '../../shared'
 import './QueryControls.css'
 
-const QueryControls = ({ setQueryResults }) => {
+const QueryControls = ({ form, handleTextChange, handleFlagChange, setQueryResults }) => {
     const { version, isLoading: versionIsLoading, isError: versionIsError } = useVersion()
-
-    const { register, handleSubmit } = useForm()
-
-    const [searchParams, setSearchParams] = useSearchParams()
-    const getStringParams = (name, defaultValue) => searchParams.has(name) ? searchParams.get(name) : defaultValue
-    const getBooleanParams = (name, defaultValue) => searchParams.has(name) ? searchParams.get(name) === 'true' : defaultValue
-
-    const [scopeIncludes, setScopeIncludes] = useState(getStringParams('scopeIncludes', '//'))
-    const handleScopeIncludes = e => setScopeIncludes(e.target.value)
-
-    const [scopeExcludes, setScopeExcludes] = useState(getStringParams('scopeExcludes', ''))
-    const handleScopeExcludes = e => setScopeExcludes(e.target.value)
-
-    const [packageScope, setPackageScope] = useState(getBooleanParams('packageScope', true))
-    const handlePackageScope = () => setPackageScope(!packageScope)
-
-    const [classScope, setClassScope] = useState(getBooleanParams('classScope', false))
-    const handleClassScope = () => setClassScope(!classScope)
-
-    const [featureScope, setFeatureScope] = useState(getBooleanParams('featureScope', false))
-    const handleFeatureScope = () => setFeatureScope(!featureScope)
-
-    const [filterIncludes, setFilterIncludes] = useState(getStringParams('filterIncludes', '//'))
-    const handleFilterIncludes = e => setFilterIncludes(e.target.value)
-
-    const [filterExcludes, setFilterExcludes] = useState(getStringParams('filterExcludes', ''))
-    const handleFilterExcludes = e => setFilterExcludes(e.target.value)
-
-    const [packageFilter, setPackageFilter] = useState(getBooleanParams('packageFilter', true))
-    const handlePackageFilter = () => setPackageFilter(!packageFilter)
-
-    const [classFilter, setClassFilter] = useState(getBooleanParams('classFilter', false))
-    const handleClassFilter = () => setClassFilter(!classFilter)
-
-    const [featureFilter, setFeatureFilter] = useState(getBooleanParams('featureFilter', false))
-    const handleFeatureFilter = () => setFeatureFilter(!featureFilter)
-
-    const [showInbounds, setShowInbounds] = useState(getBooleanParams('showInbounds', true))
-    const handleShowInbounds = () => setShowInbounds(!showInbounds)
-
-    const [showOutbounds, setShowOutbounds] = useState(getBooleanParams('showOutbounds', true))
-    const handleShowOutbounds = () => setShowOutbounds(!showOutbounds)
-
-    const [showEmptyNodes, setShowEmptyNodes] = useState(getBooleanParams('showEmptyNodes', true))
-    const handleShowEmptyNodes = () => setShowEmptyNodes(!showEmptyNodes)
-
-    const onSubmit = data => {
-        const request = new Request(QUERY_URL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-        })
-        fetch(request)
-            .then(response => response.json())
-            .then(json => setQueryResults(json))
-        setSearchParams(data)
-    }
 
     useEffect(
         () => {
-            onSubmit({
-                scopeIncludes,
-                scopeExcludes,
-                packageScope,
-                classScope,
-                featureScope,
-                filterIncludes,
-                filterExcludes,
-                packageFilter,
-                classFilter,
-                featureFilter,
-                showInbounds,
-                showOutbounds,
-                showEmptyNodes,
-            })
+            const submitForm = async () => {
+                const request = new Request(QUERY_URL, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(form),
+                })
+                const response = await fetch(request)
+                setQueryResults(await response.json())
+            }
+
+            submitForm()
         },
         // eslint-disable-next-line react-hooks/exhaustive-deps
-        [
-            // scopeIncludes,
-            // scopeExcludes,
-            packageScope,
-            classScope,
-            featureScope,
-            // filterIncludes,
-            // filterExcludes,
-            packageFilter,
-            classFilter,
-            featureFilter,
-            showInbounds,
-            showOutbounds,
-            showEmptyNodes,
-        ],
+        [form],
     )
 
     return (
         <div className="query-controls">
-            <form onSubmit={handleSubmit(onSubmit)}>
-
                 <Title />
 
                 <NavBar />
@@ -120,45 +42,43 @@ const QueryControls = ({ setQueryResults }) => {
                             <label
                                 title="Check to include matching package nodes in query results."
                                 htmlFor="packageScope">
-                                <input {...register("packageScope")} type="checkbox"
+                                <input type="checkbox"
                                        name="packageScope" id="packageScope"
-                                       checked={packageScope}
-                                       onChange={handlePackageScope}/> packages
+                                       checked={form.packageScope}
+                                       onChange={handleFlagChange}/> packages
                             </label>
                         </span>
                         <span className="scope-control">
                             <label
                                 title="Check to include matching class nodes in query results."
                                 htmlFor="classScope">
-                                <input {...register("classScope")} type="checkbox"
+                                <input type="checkbox"
                                        name="classScope" id="classScope"
-                                       checked={classScope}
-                                       onChange={handleClassScope}/> classes
+                                       checked={form.classScope}
+                                       onChange={handleFlagChange}/> classes
                             </label>
                         </span>
                         <span className="scope-control">
                             <label
                                 title="Check to include matching feature nodes in query results."
                                 htmlFor="featureScope">
-                                <input {...register("featureScope")} type="checkbox"
+                                <input type="checkbox"
                                        name="featureScope" id="featureScope"
-                                       checked={featureScope}
-                                       onChange={handleFeatureScope}/> features
+                                       checked={form.featureScope}
+                                       onChange={handleFlagChange}/> features
                             </label>
                         </span>
                         <div className="regex">
                             <label htmlFor="scopeIncludes">includes:</label>
-                            <input {...register("scopeIncludes")}
-                                   name="scopeIncludes" id="scopeIncludes"
+                            <input name="scopeIncludes" id="scopeIncludes"
                                    size={40}
-                                   defaultValue={scopeIncludes}
-                                   onChange={handleScopeIncludes}/>
+                                   value={form.scopeIncludes}
+                                   onChange={handleTextChange}/>
                             <label htmlFor="scopeExcludes">excludes:</label>
-                            <input {...register("scopeExcludes")}
-                                   name="scopeExcludes" id="scopeExcludes"
+                            <input name="scopeExcludes" id="scopeExcludes"
                                    size={40}
-                                   defaultValue={scopeExcludes}
-                                   onChange={handleScopeExcludes}/>
+                                   value={form.scopeExcludes}
+                                   onChange={handleTextChange}/>
                         </div>
                     </fieldset>
                 </div>
@@ -170,47 +90,43 @@ const QueryControls = ({ setQueryResults }) => {
                             <label
                                 title="Check to include matching package nodes in query results."
                                 htmlFor="packageFilter">
-                                <input {...register("packageFilter")}
-                                       type="checkbox"
+                                <input type="checkbox"
                                        name="packageFilter" id="packageFilter"
-                                       checked={packageFilter}
-                                       onChange={handlePackageFilter}/> packages
+                                       checked={form.packageFilter}
+                                       onChange={handleFlagChange}/> packages
                             </label>
                         </span>
                         <span className="filter-control">
                             <label
                                 title="Check to include matching class nodes in query results."
                                 htmlFor="classFilter">
-                                <input {...register("classFilter")} type="checkbox"
+                                <input type="checkbox"
                                        name="classFilter" id="classFilter"
-                                       checked={classFilter}
-                                       onChange={handleClassFilter}/> classes
+                                       checked={form.classFilter}
+                                       onChange={handleFlagChange}/> classes
                             </label>
                         </span>
                         <span className="filter-control">
                             <label
                                 title="Check to include matching feature nodes in query results."
                                 htmlFor="featureFilter">
-                                <input {...register("featureFilter")}
-                                       type="checkbox"
+                                <input type="checkbox"
                                        name="featureFilter" id="featureFilter"
-                                       checked={featureFilter}
-                                       onChange={handleFeatureFilter}/> features
+                                       checked={form.featureFilter}
+                                       onChange={handleFlagChange}/> features
                             </label>
                         </span>
                         <div className="regex">
                             <label htmlFor="filterIncludes">includes:</label>
-                            <input {...register("filterIncludes")}
-                                   name="filterIncludes" id="filterIncludes"
+                            <input name="filterIncludes" id="filterIncludes"
                                    size={40}
-                                   defaultValue={filterIncludes}
-                                   onChange={handleFilterIncludes}/>
+                                   value={form.filterIncludes}
+                                   onChange={handleTextChange}/>
                             <label htmlFor="filterExcludes">excludes:</label>
-                            <input {...register("filterExcludes")}
-                                   name="filterExcludes" id="filterExcludes"
+                            <input name="filterExcludes" id="filterExcludes"
                                    size={40}
-                                   defaultValue={filterExcludes}
-                                   onChange={handleFilterExcludes}/>
+                                   value={form.filterExcludes}
+                                   onChange={handleTextChange}/>
                         </div>
                     </fieldset>
                 </div>
@@ -222,49 +138,43 @@ const QueryControls = ({ setQueryResults }) => {
                             <label
                                 title="Check to include matching package nodes in query results."
                                 htmlFor="showInbounds">
-                                <input {...register("showInbounds")}
-                                       type="checkbox"
+                                <input type="checkbox"
                                        name="showInbounds" id="showInbounds"
-                                       checked={showInbounds}
-                                       onChange={handleShowInbounds}/> {INBOUND}
+                                       checked={form.showInbounds}
+                                       onChange={handleFlagChange}/> {INBOUND}
                             </label>
                         </span>
                         <span className="show-control">
                             <label
                                 title="Check to include matching package nodes in query results."
                                 htmlFor="showOutbounds">
-                                <input {...register("showOutbounds")}
-                                       type="checkbox"
+                                <input type="checkbox"
                                        name="showOutbounds" id="showOutbounds"
-                                       checked={showOutbounds}
-                                       onChange={handleShowOutbounds}/> {OUTBOUND}
+                                       checked={form.showOutbounds}
+                                       onChange={handleFlagChange}/> {OUTBOUND}
                             </label>
                         </span>
                         <span className="show-control">
                             <label
                                 title="Check to include matching package nodes in query results."
                                 htmlFor="showEmptyNodes">
-                                <input {...register("showEmptyNodes")}
-                                       type="checkbox"
+                                <input type="checkbox"
                                        name="showEmptyNodes" id="showEmptyNodes"
-                                       checked={showEmptyNodes}
-                                       onChange={handleShowEmptyNodes}/> empty nodes
+                                       checked={form.showEmptyNodes}
+                                       onChange={handleFlagChange}/> empty nodes
                             </label>
                         </span>
                     </fieldset>
                 </div>
                 {(!versionIsLoading && !versionIsError) && <div className="footnote">Use Perl regular expressions, <Link to={version.implementation.url + 'Manual.html#PerlRegularExpressions'}>see the manual</Link>.</div>}
-
-                <span className="submit">
-                    <button type="submit" className="query-controls-submit">Run Query</button>
-                </span>
-
-            </form>
         </div>
     )
 }
 
 QueryControls.propTypes = {
+    form: PropTypes.object.isRequired,
+    handleTextChange: PropTypes.func.isRequired,
+    handleFlagChange: PropTypes.func.isRequired,
     setQueryResults: PropTypes.func.isRequired,
 }
 
